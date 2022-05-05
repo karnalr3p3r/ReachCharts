db5fbceba32594cc9130eeeee2fd1fc23b0c4314
karnalr3p3r <devereauxma97@gmail.com>
2022-05-04 20:52:01 -0500

    updated code

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import DeviceChart from "./DeviceChart";
 import Layout from "./Layout";
+import { name } from "../utils/generators";
 
 export default function Home() {
   return (
     <Layout>
+      <h2>{name("Jacob", "Devereaux")}</h2>
       <DeviceChart
         options={{
           colors: ["#0f6375", "#f5a623", "#f5a623"],
