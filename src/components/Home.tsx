bae387c85c690e159ce506bd14288fa2464e10c0
karnalr3p3r <devereauxma97@gmail.com>
2022-05-04 20:49:52 -0500

    color change

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -5,7 +5,19 @@ import Layout from "./Layout";
 export default function Home() {
   return (
     <Layout>
-      <DeviceChart />
+      <DeviceChart
+        options={{
+          colors: ["#0f6375", "#f5a623", "#f5a623"],
+          labels: ["Desktop", "Mobile", "Tablet"],
+        }}
+      />
+
+      <DeviceChart
+        options={{
+          colors: ["#c00", "#3469a7", "#c1b01e"],
+          labels: ["Smartphones", "laptops", "ipads"],
+        }}
+      />
     </Layout>
   );
 }
